6e576691bfc667a439b69ed193f45c4cc3309dc2
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-12-11 16:32:49 -0300

    Remove constructor and methods from Professor class
    resumida

--- Professor.java
+++ Professor.java
@@ -1,16 +1,4 @@
 public class Professor extends Pessoa {
     private String siape;
     private String formacao;
-
-    public Professor(String nome, String nascimento, String siape, String formacao) {
-        super(nome, nascimento);
-        this.siape = siape;
-        this.formacao = formacao;
-    }
-
-    public String getSiape() { return siape; }
-    public void setSiape(String siape) { this.siape = siape; }
-
-    public String getFormacao() { return formacao; }
-    public void setFormacao(String formacao) { this.formacao = formacao; }
 }
