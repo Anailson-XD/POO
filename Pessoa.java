ef7432cb59cb1ccf70baa87739bb8a30c5aa1e4e
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-12-11 16:30:50 -0300

    Refactor Pessoa class by removing unnecessary methods
    resumida

--- Pessoa.java
+++ Pessoa.java
@@ -1,16 +1,6 @@
 public abstract class Pessoa {
     private String nome;
-    private String nascimento; // formato simples, p.ex. "2001-05-20"
+    private String nascimento;
 
-    public Pessoa(String nome, String nascimento) {
-        this.nome = nome;
-        this.nascimento = nascimento;
-    }
-
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-
-    public String getNascimento() { return nascimento; }
-    public void setNascimento(String nascimento) { this.nascimento = nascimento; }
+    // getters e setters
 }
-
