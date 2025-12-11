e405f746e0ebf6dfba9f288c716f9c0ffbcabdfc
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-12-11 16:32:02 -0300

    Remove disciplina management from Aluno class
    resumida

--- Aluno.java
+++ Aluno.java
@@ -1,26 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Aluno extends Pessoa {
     private String matricula;
-    private final List<Disciplina> disciplinas = new ArrayList<>();
-
-    public Aluno(String nome, String nascimento, String matricula) {
-        super(nome, nascimento);
-        this.matricula = matricula;
-    }
-
-    public String getMatricula() { return matricula; }
-    public void setMatricula(String matricula) { this.matricula = matricula; }
-
-    public List<Disciplina> getDisciplinas() { return disciplinas; }
-
-    public void adicionarDisciplina(Disciplina d) {
-        if (d == null) return;
-        disciplinas.add(d);
-    }
-
-    public void removerDisciplina(Disciplina d) {
-        disciplinas.remove(d);
-    }
 }
