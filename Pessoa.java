1300d65c06f25703bf38be5b8c5e9fab8badeb95
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-12-15 13:57:48 -0300

    resumida

--- Pessoa.java
+++ Pessoa.java
@@ -2,5 +2,9 @@ public abstract class Pessoa {
     private String nome;
     private String nascimento;
 
-    // getters e setters
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getNascimento() { return nascimento; }
+    public void setNascimento(String nascimento) { this.nascimento = nascimento; }
 }
