39657f8dad62988f9d33b14c6195038d224149a9
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-12-15 14:01:24 -0300

    Delete MediaAritmetica.java

--- MediaAritmetica.java
+++
@@ -1,9 +0,0 @@
-public class MediaAritmetica implements CalcularMedia {
-    @Override
-    public double calc(double... notas) {
-        if (notas == null || notas.length == 0) return 0.0;
-        double soma = 0.0;
-        for (double n : notas) soma += n;
-        return soma / notas.length;
-    }
-}
