c18b0c0f80ab0cd3963feb5243ebc8892a51180a
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-12-15 13:58:33 -0300

    Add getters and setters for siape and formacao

--- Professor.java
+++ Professor.java
@@ -1,4 +1,10 @@
 public class Professor extends Pessoa {
     private String siape;
     private String formacao;
+
+    public String getSiape() { return siape; }
+    public void setSiape(String siape) { this.siape = siape; }
+
+    public String getFormacao() { return formacao; }
+    public void setFormacao(String formacao) { this.formacao = formacao; }
 }
