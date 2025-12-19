a5360a123e3cc3e87d3a35b6a7ee3f731e80ae31
Anailson-XD <anailson.lima33@aluno.ifce.edu.br>
2025-12-19 11:49:41 -0300

    Delete Aluno.java

--- Aluno.java
+++
@@ -1,6 +0,0 @@
-public class Aluno extends Pessoa {
-    private String matricula;
-
-    public String getMatricula() { return matricula; }
-    public void setMatricula(String matricula) { this.matricula = matricula; }
-}
